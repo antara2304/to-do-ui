5f418549e53af8b4bb9670cd0ccb03783889a46d
antara <antara.das.dev@gmail.com>
2024-01-28 12:08:55 +0530

    Show name on the sidebar

--- src/app/layout/side-nav/side-nav.component.ts
+++ src/app/layout/side-nav/side-nav.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TokenService } from '@app/data/services/api/token.service';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
 })
-export class SideNavComponent {
-  constructor(private router: Router, private route: ActivatedRoute) {}
+export class SideNavComponent implements OnInit {
+  public userName: string = '';
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private tokenSvc: TokenService
+  ) {}
+  ngOnInit() {
+    try {
+      this.userName = this.tokenSvc.decodeToken()['name'];
+    } catch (error) {
+      console.error(error);
+    }
+  }
   logout() {
     localStorage.removeItem('token');
     this.router.navigate(['/sign-in'], { relativeTo: this.route });
